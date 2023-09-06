d4acdacb75885bf33fe384f6094df268c29c1064
Uíza Silva <uizacarvalhosilva@gmail.com>
2023-09-06 19:32:07 -0300

    Ajustando o while e times

---
+++ time.rb
@@ -0,0 +1,8 @@
+6.times {print "Aprendendo o times "}
+3.times {puts "Aprendendo o times"}
+
+
+
+20.times do |contador|
+    puts "Aprendendo o times #{contador}"
+end
---
+++ while.rb
@@ -0,0 +1,6 @@
+num = 0
+
+while num <= 100
+    puts num
+    num += 1
+end
